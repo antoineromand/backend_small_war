68e442513d84f48347607f04f2f685cc4c1f3220
antoineromand <antoine.romand@epitech.eu>
2022-06-28 18:34:28 +0200

    Game Crud : new Game and end Game

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GameService } from './game.service';
 import { GameController } from './game.controller';
+import { Game } from 'src/Model/Game.entity';
+import { ResultGame } from 'src/Model/ResultGame.entity';
+import { Player } from 'src/Model/Player.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Game, ResultGame, Player])],
   controllers: [GameController],
-  providers: [GameService]
+  providers: [GameService],
+  exports: [GameService],
 })
 export class GameModule {}
