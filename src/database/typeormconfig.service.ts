3cad357bd641b786fd63d6ece338a8ca22d65613
antoineromand <antoine.romand@epitech.eu>
2022-07-11 11:31:47 +0200

    add new env variable for prod and dev, change typeorm config service for prod

--- src/database/typeormconfig.service.ts
+++ src/database/typeormconfig.service.ts
@@ -9,11 +9,11 @@ export class TypeormConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'mysql',
-      host: process.env.SW_HOST,
+      host: process.env.SW_HOST_PROD,
       port: parseInt(process.env.SW_PORT),
-      username: process.env.SW_ADMIN,
-      password: process.env.SW_PASSWORD,
-      database: process.env.SW_DATABASE,
+      username: process.env.SW_ADMIN_PROD,
+      password: process.env.SW_PASSWORD_PROD,
+      database: process.env.SW_DATABASE_PROD,
       entities: ['dist/**/*.entity.js'],
       synchronize: true,
     };
